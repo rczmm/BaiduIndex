7919402b2f30b032fba877bde3d14240109a8237
rczmm <3179623450@qq.com>
2024-05-02 02:42:15 +0800

    修改

--- demo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-values = [np.random.randint(0, 100) for i in range(100)]
-print(values)
-print(type(values))
-
-values = np.array(values)
-min_value = np.min(values)
-max_value = np.max(values)
